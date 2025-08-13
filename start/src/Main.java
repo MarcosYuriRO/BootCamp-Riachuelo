0a9868f54f88060b03a595730f72972f8c394096
Marcos Yuri <marcos.yuriro@gmail.com>
2025-08-13 11:34:57 -0300

    Add files via upload

--- start/src/Main.java
+++ start/src/Main.java
@@ -5,10 +5,19 @@ public class Main {
         //Operadores de Atribuição e Lógicos:
         Scanner scanner = new Scanner(System.in);
         System.out.println("Quanl a sua idade");
-        var age = scanner.nextInt();
+        int age = scanner.nextInt();
         System.out.println("Você é Emancipado(a)?");
-        var isEmancipated = scanner.nextBoolean();
-        var canDrive = age >= 18 || (isEmancipated && age >= 16);
+        boolean isEmancipated = scanner.nextBoolean();
+        boolean canDrive = age >= 18 || (isEmancipated && age >= 16);
         System.out.printf("Você pode dirigir? (%s) \n", canDrive);
+
+        //Operadores Aritméticos:
+        System.out.println("Digite um número:");
+        int value1 = scanner.nextInt();
+        System.out.printf("A raíz de %d é %.2f\n", value1, Math.sqrt(value1));
+
+        System.out.println("Digite outro número:");
+        int value2 = scanner.nextInt();
+        System.out.printf("A potência de %d é igual a %.2f\n", value2, Math.pow(value2, 2));
     }
 }
